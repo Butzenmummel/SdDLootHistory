f2555e78696f6c5b0d0a158b0e95d290933e2e7b
Alexander Teichmann <teichmann.alex.mss@gmail.com>
2024-10-05 17:21:37 +0200

    init

---
+++ data.js
@@ -0,0 +1,19 @@
+const lootData = [
+    {
+        "player": "Ratatatt-Blackmoore",
+        "itemName": "Dunkles Antlitz des Jenseits",
+        "class": "WARLOCK",
+        "boss": "Der blutgebundene Schrecken",
+        "response": "Persönliche Beute – Nicht handelbar",
+        "votes": 0
+    },
+    {
+        "player": "Embo-Blackmoore",
+        "itemName": "Leuchtfeuer der trügerischen Hoffnung",
+        "class": "HUNTER",
+        "boss": "Sikran, Hauptmann der Sureki",
+        "response": "Major",
+        "votes": 0
+    },
+    // (continue with other entries)
+];
